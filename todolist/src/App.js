135243d5e3a2c82a2abe64aafa182b2553049b35
JeanFrangeul <frangeuljean@gmail.com>
2022-01-24 14:29:08 +0100

    affichage de la liste todos

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,19 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import store from './store';
 
 function App() {
   const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    store.subscribe(() =>  {
+      setTodos(store.getState().todos);
+    });
+  }, [])
 
   const changeHandler = event => {
     setTodo(event.target.value);
+  };
+
+  const submitHandler = event =>{
+    event.preventDefault();
+
+    if (todo.trim().length === 0) {
+      return;
+    }
+    store.dispatch({
+      type: 'ADD_TODO',
+      payload: todo.trim()
+    });
+
+    setTodo('');
   }
 
   return (
     <div>
-      <form>
+      <form onSubmit={submitHandler}>
         <input type="text" value={todo} onChange={changeHandler} />
         <button>Add</button>
       </form>
+      <ol>
+        {todos.map(todo => {
+          return <li>{todo}</li>
+        })}
+      </ol>
     </div>
   );
 }
