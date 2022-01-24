111fe17ec79d9ddac62852e7efdf0097511f8e52
JeanFrangeul <frangeuljean@gmail.com>
2022-01-24 14:09:30 +0100

    depart todolist form

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,49 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import store from './store';
 
 function App() {
   const [todo, setTodo] = useState('');
-  const [todos, setTodos] = useState([]);
 
-  useEffect(() => {
-    store.subscribe(() => {
-      setTodos(store.getState().todos)
-    })
-  }, []);
-
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-
-    if (todo.trim().length === 0) {
-      return;
-    }
-
-    store.dispatch({
-      type: 'ADD_TODO',
-      payload: todo.trim()
-    });
-
-    setTodo('')
-  }
-
-  const changeHandler = (event) => {
+  const changeHandler = event => {
     setTodo(event.target.value);
   }
 
   return (
-    <div className="App">
-      <form onSubmit={submitHandler}>
-        <input type="text" value={todo} onChange={changeHandler}/>
+    <div>
+      <form>
+        <input type="text" value={todo} onChange={changeHandler} />
         <button>Add</button>
       </form>
-      <ol>
-        {todos.map(todo => {
-          return <li>{todo}</li>
-        })}
-      </ol>
-
     </div>
   );
 }
